de2c3615512239a357011b4909f99c39a9d95640
Punit Khinchi <punitkhinchi@gmail.com>
2026-09-23 15:56:27 +0530

    Cleanup remaining temp files

--- fix4.ps1
+++
@@ -1,3 +0,0 @@
-﻿$js = Get-Content 'src\js\main.js' -Encoding UTF8
-$js = $js | Where-Object { -not ($_ -match 'Open pricing' -or $_ -match 'Try NEXUS Pro') }
-Set-Content 'src\js\main.js' -Value $js -Encoding UTF8
